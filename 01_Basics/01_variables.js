33d348665e2533be074d5217c901e1de0e7bcc75
Adhiraj Pratap singh <adhirajpratapsingh079@gmail.com>
2026-09-01 18:40:46 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 998987
+let accountEmail= "pratapsingh079@gmil.com"
+var accountPassword = "12345"
+accountCity="lucknow"
+let accountState;
+
+//accountId=2
+
+accountEmail="hcl123@gmail.com"
+accountPassword="5656"
+accountCity="Barabanki"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version
+
+// alert("Adhiraj") we are using node js not browser
+
+console.log(3+3);
+console.log("Adhiraj")
+
+
+let name="Adhiraj" //string datatype
+let age =20 // number
+let isLoggedIn = false // boolean7
+
+console.table([name,age,isLoggedIn])
+
+/* number =>2^53
+bigint
+string=>""
+boolean=>true/false
+null=>standalone value
+undefined
+symbol=>unique
+*/
+
+console.log(typeof name)
+
